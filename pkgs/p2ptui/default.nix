2a4a513b989cee5b6df40c74ef07e59a3254973f
KristianSvanholm <KristianRorenSvanholm@gmail.com>
2025-05-02 00:07:56 +0200

    vendor hash p2p

--- pkgs/p2ptui/default.nix
+++ pkgs/p2ptui/default.nix
@@ -14,6 +14,8 @@ buildGoModule rec {
     hash = "sha256-K+0aGTBtE7liLSyUhd14aDBO3qBbyJBUjajrVbAt7FI=";
   };
 
+  vendorHash = "sha256-Nzh8EpFGg00jBWxkhk3TYmnHzayANe2rJvsZu2g+IAM=";
+
   meta = {
     description = "Little minesweeper for the terminal with p2p multiplayer capabilities.";
     homepage = "https://github.com/Dreamacro/go-check";
